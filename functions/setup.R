71c6a1c3c027276da6ebf1b8b57066f2151dd71c
nikof7 <wjuice7@gmail.com>
2024-06-17 11:55:14 -0300

    First commit

---
+++ functions/setup.R
@@ -0,0 +1,15 @@
+# Lista de paquetes necesarios
+packages_ <- c("tidyverse", "bookdown", "knitr", "rmarkdown", "suncalc", "circular", "sp", "activity", "NPCirc")
+
+# Función para instalar y cargar paquetes
+install_and_load <- function(packages_) {
+  for (package_ in packages_) {
+    if (!require(package_, character.only = TRUE)) {
+      install.packages(package_, dependencies = TRUE)
+      library(package_, character.only = TRUE)
+    }
+  }
+}
+
+# Instalar y cargar paquetes
+install_and_load(packages_)
